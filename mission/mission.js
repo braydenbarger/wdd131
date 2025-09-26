d2161cfc65e29040aef4b235409c545e4274b160
braydenbarger <bar21065@byui.edu>
2025-09-25 15:07:45 -1000

    Week 2

---
+++ mission/mission.js
@@ -0,0 +1,15 @@
+const themeSelector = document.querySelector('#theme-select');
+
+function changeTheme() {
+    const logo = document.querySelector('.logo');
+    
+    if (themeSelector.value === 'dark') {
+        document.body.classList.add('dark');
+        logo.src = 'byui-logo_white.webp';
+    } else {
+        document.body.classList.remove('dark');
+        logo.src = 'byui-logo_blue.webp';
+    }
+}
+
+themeSelector.addEventListener('change', changeTheme);
