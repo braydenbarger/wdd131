ec0c79ba48f1fe2a88a2d91040c53b5d43ba55a1
braydenbarger <bar21065@byui.edu>
2025-10-11 15:05:42 -1000

    W04

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,6 +1,28 @@
-const menuButton = document.getElementById("menuButton");
-const navMenu = document.getElementById("navMenu");
+const gallery = document.querySelector(".gallery");
+const modal = document.getElementById("modal");
+const modalImage = document.getElementById("modalImage");
+const closeBtn = document.querySelector(".close-viewer");
 
-menuButton.addEventListener("click", () => {
-  navMenu.classList.toggle("hidden");
+gallery.addEventListener("click", (event) => {
+  const clickedImage = event.target.closest("img");
+  if (!clickedImage) return;
+
+  const src = clickedImage.getAttribute("src");
+  const alt = clickedImage.getAttribute("alt");
+
+  const fullSrc = src.replace("-small", "-full");
+
+  modalImage.src = fullSrc;
+  modalImage.alt = alt;
+  modal.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.close();
+});
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.close();
+  }
 });
