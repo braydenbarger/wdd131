baae6e6b7292cca7415ba08fcac17008496fc5e0
braydenbarger <bar21065@byui.edu>
2025-10-24 20:10:56 -1000

    W06

--- blog/blog.js
+++ blog/blog.js
@@ -3,6 +3,7 @@ const articles = [
 	  id: 1,
 	  title: "Septimus Heap Book One: Magyk",
 	  date: "July 5, 2022",
+	  isoDate: "2022-07-05",
 	  description: "If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.",
 	  imgSrc: "https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg",
 	  imgAlt: "Book cover for Septimus Heap 1",
@@ -14,6 +15,7 @@ const articles = [
 	  id: 2,
 	  title: "Magnus Chase Book One: Sword of Summer",
 	  date: "December 12, 2021",
+	  isoDate: "2021-12-12",
 	  description: "The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.",
 	  imgSrc: "https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300",
 	  imgAlt: "Book cover for Magnus Chase 1",
@@ -25,6 +27,7 @@ const articles = [
 	  id: 3,
 	  title: "Belgariad Book One: Pawn of Prophecy",
 	  date: "Feb 12, 2022",
+	  isoDate: "2022-02-12",
 	  description: "A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
 	  imgSrc: "https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
 	  imgAlt: "Book cover for Pawn of Prophecy",
@@ -43,7 +46,7 @@ const articles = [
   
 	  articleEl.innerHTML = `
 		<div class="book-details">
-		  <time datetime="${article.date}">${article.date}</time>
+		  <time datetime="${article.isoDate}">${article.date}</time>
 		  <p>${article.ages}</p>
 		  <p>${article.genre}</p>
 		  <p>${article.stars}</p>
